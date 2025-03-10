a6e97d35d8fc61dc0b8896a6257b295a63a6574d
NP <167005471+Figaro45@users.noreply.github.com>
2025-03-10 19:35:19 +0300

    Media_Slaider_One

--- js/slaider.js
+++ js/slaider.js
@@ -1,3 +1,40 @@
-let swiper = new Swiper('.swiper', {
+let swiper01 = new Swiper('.block_swiper01', {
+    slidesPerView: 4,
+    spaceBetween: 1,
+    breakpoints: {
+        1500: {
+            slidesPerView: 4,
+            spaceBetween: 1,
+        },
+        1100: {
+            slidesPerView: 3,
+            spaceBetween: 1,
+        },
+        700: {
+            slidesPerView: 2.5,
+            spaceBetween: 1,
+        },
+        600: {
+            slidesPerView: 2.9,
+            spaceBetween: 1,
+        },
+        500: {
+            slidesPerView: 2.8,
+            spaceBetween: 1,
+        },
+        300: {
+            slidesPerView: 1.7,
+            spaceBetween: 1,
+        },
+        1: {
+            slidesPerView: 1.5,
+            spaceBetween: 1,
+        },
+    }
+});
+
+
+let swiper02 = new Swiper('.block_swiper02', {
     slidesPerView: 0.5,
 });
+
